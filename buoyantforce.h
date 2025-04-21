5e76b7e42c13b0905de334ccf9d314d2e095d07f
Rajesh Srivatsav <rajeshsrivatsav@Rajeshs-MacBook-Air.local>
2025-04-20 22:35:29 -0500

    Moving files into a new directory

--- buoyantforce.h
+++
@@ -1,4 +0,0 @@
-#include <iostream>
-#include "parameters.h"
-
-void buoyantforce(float (&) [XDIM][YDIM][ZDIM],float (&) [XDIM][YDIM][ZDIM],float (&) [XDIM][YDIM][ZDIM]);
--- grid.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <vector>
-
-class Grid{
-    std::vector<double> data;
-    void fill(double value) {
-        std::fill(data.begin(), data.end(), value);
-    }
-};
