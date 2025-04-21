3745d9b7aa9e2f6f75e8df3353adfbd02e122975
Rajesh Srivatsav <rajeshsrivatsav@Rajeshs-MacBook-Air.local>
2025-04-20 22:34:14 -0500

    OpenMP implementation - Initial snapshot

---
+++ openmp_impl/Utilities.h
@@ -0,0 +1,11 @@
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <stdexcept>
+#include "parameters.h"
+
+void Clear(float (&x)[XDIM][YDIM][ZDIM]);
+void InitializeProblem(float (&x)[XDIM][YDIM][ZDIM], float (&b)[XDIM][YDIM][ZDIM], float (&c)[XDIM][YDIM][ZDIM]);
+void writetoCSV(float (&x)[XDIM][YDIM][ZDIM], const std::string& filename, const std::string& ftype);
+float laplacian(float (&p)[XDIM][YDIM][ZDIM], int i, int j, int k);
---
+++ openmp_impl/cga.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "parameters.h"
+#include "Utilities.h"
+
+void solvePressureCG(
+    float (&p)[XDIM][YDIM][ZDIM],
+    float (&b)[XDIM][YDIM][ZDIM]
+);
