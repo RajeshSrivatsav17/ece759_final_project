0573be46e9f7d00e3387bbc7ce98fa7338b44391
JP-Reddy <guthijp.reddy@gmail.com>
2025-05-03 13:51:16 -0500

    utilities fix

--- cuda_shared_mem/utilities.cuh
+++ cuda_shared_mem/utilities.cuh
@@ -4,9 +4,9 @@
 #include "parameters.h"
 
 // Function to clear a 3D array
-__host__ void Clear(float (&arr)[XDIM][YDIM][ZDIM]);
+inline __host__ void Clear(float (&arr)[XDIM][YDIM][ZDIM]);
 
 // Function to initialize the problem with density, temperature, and velocity
-__host__ void InitializeProblem(float (&rho)[XDIM][YDIM][ZDIM], float (&T)[XDIM][YDIM][ZDIM], float (&v)[XDIM][YDIM][ZDIM]);
+inline __host__ void InitializeProblem(float (&rho)[XDIM][YDIM][ZDIM], float (&T)[XDIM][YDIM][ZDIM], float (&v)[XDIM][YDIM][ZDIM]);
 
 #endif // UTILITIES_H
